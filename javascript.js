93e3eb5634d75eb33da13f443f64aec5d9dba0a8
Tyler <nguyentyler97@gmail.com>
2022-07-30 09:32:59 -0700

    Comment basic code skeleton

---
+++ javascript.js
@@ -0,0 +1,10 @@
+// Add function
+// Subtract function
+// Multiply function
+// Divide function
+
+// Operate function that takes 2 numbers and calls one of the above functions
+// Use the array reduce function to make mutiple calculations
+// Answers with long decimals should be rounded
+// Clicking = before entering all numbers should cause an error
+// Dividing by 0 should return an error message
