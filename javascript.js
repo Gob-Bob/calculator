d52f79f335a49d0f30bd9aec322b93bf4ffbdeda
Tyler <nguyentyler97@gmail.com>
2022-08-01 17:19:36 -0700

    Add basic arithmetic functions

--- javascript.js
+++ javascript.js
@@ -1,7 +1,10 @@
 // Add function
+const add = (a, b) => a + b;
 // Subtract function
 // Multiply function
 // Divide function
+
+console.log(add(1, 3));
 
 // Operate function that takes 2 numbers and calls one of the above functions
 // Use the array reduce function to make mutiple calculations
